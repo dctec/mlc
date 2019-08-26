98c798762ada6a159b2a6f3966f693736a85bae7
dctec <mlc@digitalfuss.net>
2019-08-26 18:23:52 -0500

    step 2. Not working yet. Error in JS webCam object

--- 04-tensorflow-js/index.js
+++ 04-tensorflow-js/index.js
@@ -1,5 +1,7 @@
 let net;
 
+// STEP 1 of tutorial
+/*
 async function app() {
   console.log('Loading mobilenet..');
 
@@ -11,6 +13,51 @@ async function app() {
   const imgEl = document.getElementById('img');
   const result = await net.classify(imgEl);
   console.log(result);
+}
+*/
+
+
+// STEP TWO of Tutorial: setup webcam AND remove prediction through image and instead make predictions through content from webcam
+// https://codelabs.developers.google.com/codelabs/tensorflowjs-teachablemachine-codelab/index.html#5
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+  
+  await setupWebcam();
+  while (true) {
+    const result = await net.classify(webcamElement);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
 }
 
 app();
