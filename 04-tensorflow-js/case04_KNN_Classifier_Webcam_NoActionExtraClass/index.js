b0a148b39b8008f025f1087b98b2ef1e5e9b1ff7
dctec <mlc@digitalfuss.net>
2019-08-31 07:27:27 -0500

    Labeled -no action- by detecting low probabilities of other three classes but seems slow to identity this to the user and when it does, it changes really quick back to classifying the current image as one of the other classes. Tried training 10 samples for each category but still gives a high probability of any of the three at any time regardless. Will move approach to now create another class and capture initial background to it

--- 04-tensorflow-js/case04_KNN_Classifier_Webcam_NoActionExtraClass/index.js
+++ 04-tensorflow-js/case04_KNN_Classifier_Webcam_NoActionExtraClass/index.js
@@ -1,27 +1,68 @@
-// STEP 7 of tutorial. KNN Classifier
-// https://codelabs.developers.google.com/codelabs/tensorflowjs-teachablemachine-codelab/index.html#6
+// STEP 7 of tutorial. KNN Classifier with 'No Action' class
+// https://codelabs.developers.google.com/codelabs/tensorflowjs-teachablemachine-codelab/index.html#7
 
-// Create classifier (knnClassifier module is automatically included when loading knn-classifier through html scipt tag
-//  -- returns KNNImageClassifier
+// Q: What is an 'activation' and a 'promise'? I see those two mentioned often
+// but not clearly understand the origin and meaning. -dctec
+//
+// Q: What's the difference between .infer and .classify . I read that .infer 
+// is used in transfer learning but I need more info on the differences and 
+// when to use each. -dctec
+
+// knnClassifier.create()
+//    Create classifier (knnClassifier module is automatically included when 
+//    loading knn-classifier through html scipt tag
+//      -- returns KNNImageClassifier
 const classifier = knnClassifier.create();
 
 const webcamElement = document.getElementById('webcam');
 
 let net;
+let cnt_capture=[];
+let my_prediction;
+let my_probability;
+const MAX_CAPTURE_SEQ_CNT = 5;
+const classes = ['A', 'B', 'C'];
 
+// function sleep
+//   - input: milliseconds integer
+//   From https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve,ms));
+}
 
 async function app() {
   console.log('Loading mobilenet..');
 
-  // Load the model.
   net = await mobilenet.load();
+  // Notice that 'await' is only valid in an async function
+
   console.log('Sucessfully loaded model');
 
   await setupWebcam();
+  console.log('Sucessfully set Webcam up');
 
-  // Reads an image from the webcam and associates it with a specific class
-  // index.
-  const addExample = classId => {
+  // init cnt array to keep track of captured samples per classId
+  
+  for(i=0;i<classes.length;i++){
+    cnt_capture[i]=0;
+  }
+  
+
+  // function addExample(classId)
+  //   Reads an image from the webcam and associates it with a specific class
+  //   index.
+  // Shortened (arrow function) syntax as per (https://babeljs.io/) and 
+  // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions#Shorter_functions)
+  //  ...
+  //    const addExample = classId => {
+  // vs regular old fashion legible at all levels syntax:
+  //    var addExample = function addExample(classId) {
+  //
+  var addExample = function addExample(classId) {
+    var activation;
+    var i;
+
+    //
     // Get the intermediate activation of MobileNet 'conv_preds' and pass that
     // to the KNN classifier.
     // (https://blog.understand.ai/tensorflow-js-a-painless-way-to-get-started-with-machine-learning-470c15f0f637)
@@ -39,7 +80,7 @@ async function app() {
     //              img: A tensor or an image element to make a classification on
     //              embedding: If true, it returns the embedding. Otherwise it
     //                          returns the 1000-dim unnormalized logits.
-    const activation = net.infer(webcamElement, 'conv_preds');
+    // const activation = net.infer(webcamElement, 'conv_preds');
 
     // Pass the intermediate activation to the classifier.
     // API doc: https://github.com/tensorflow/tfjs-models/tree/master/knn-classifier
@@ -48,10 +89,28 @@ async function app() {
     // where
     //    example: An example to add to the dataset, usually an activation from another model
     //    label: The label (class name) of the example
-    classifier.addExample(activation, classId);
+    // classifier.addExample(activation, classId);
+
+
+    // For each click, capture N sequential images from input. So, that the
+    // the classId training converges quickly on this image. Otherwise it takes
+    // 3 clicks before converging (0.33 , 0.66, 1)
+    for(i=0;i<MAX_CAPTURE_SEQ_CNT;i++){
+        activation = net.infer(webcamElement, 'conv_preds');
+        classifier.addExample(activation, classId);
+        cnt_capture[classId]+=1;
+
+        document.getElementById('console2').innerText = ``;
+        for(j=0;j<classes.length;j++){
+          document.getElementById('console2').innerText += `
+            training count class ${classes[j]} : ${cnt_capture[j]}\n
+          `;
+        }
+    }
   };
 
   // When clicking a button, add an example for that class.
+  // document.getElemebtById('class-a').addEventListener('click', function () return addExample(0);
   document.getElementById('class-a').addEventListener('click', () => addExample(0));
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
   document.getElementById('class-c').addEventListener('click', () => addExample(2));
@@ -81,13 +140,31 @@ async function app() {
       //   confidences: maps each label to their confidence score.
       const result = await classifier.predictClass(activation);
 
-      const classes = ['A', 'B', 'C','No Action'];
-      document.getElementById('console').innerText = `
-        prediction: ${classes[result.classIndex]}\n
-        probability: ${result.confidences[result.classIndex]}
-      `;
+
+      my_prediction = classes[result.classIndex]
+      my_probability = result.confidences[result.classIndex]
+
+
+      if (my_probability < .9) {
+        document.getElementById('console').innerText = `
+          probability: ${my_probability}\n
+          ...I am waiting for you take some action and let me predict the classification of an object\n
+        `;
+        // wait before moving to next frame and updating text on screen or else
+        // the text could change so fast and be annoying to user
+        await sleep(3000);
+       } else {
+          document.getElementById('console').innerText = `
+              prediction: ${my_prediction}\n
+              probability: ${my_probability}
+          `;
+       } 
+  
     }
 
+
+    // tf.nextFrame() returns a promise that resolve when a requestAnimationFrame has 
+    // completed (https://js.tensorflow.org/api/latest/#nextFrame)
     await tf.nextFrame();
   }
 }
